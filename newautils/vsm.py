2e658bab4bdc8a3a6af79ae9ac2c1f5ec0eabb12
Sujal Bajracharya <sujalbajracharya1@gmail.com>
2025-10-30 15:56:50 +0000

    muril and tfidf run

--- newautils/vsm.py
+++ newautils/vsm.py
@@ -5,7 +5,6 @@ from . import tokenizer
 
 
 def compute_tfidf(corpus):
-
     vectorizer = TfidfVectorizer( tokenizer=tokenizer.tokenize, token_pattern=None, use_idf=True, sublinear_tf=True)
 
     tfidf_matrix = vectorizer.fit_transform(corpus["text"].tolist())
@@ -32,3 +31,14 @@ def search(query, tfidf_matrix, vectorizer, top_k=10):
     similarities = cosine_similarity(query_vector, tfidf_matrix)[0]
     top_indices = similarities.argsort()[-top_k:][::-1]
     return [(idx, similarities[idx]) for idx in top_indices]
+
+import pickle
+import scipy.sparse as sp
+
+def load_tfidf_model(matrix_path="models/tfidf_matrix.npz", vectorizer_path="models/vectorizer.pkl"):
+    tfidf_matrix = sp.load_npz(matrix_path)
+
+    with open(vectorizer_path, "rb") as f:
+        vectorizer = pickle.load(f)
+    
+    return tfidf_matrix, vectorizer
